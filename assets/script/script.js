e65f1cc18486da1089690976b579358be1454a2b
Rchandler1 <98798057+Rchandler1@users.noreply.github.com>
2022-03-26 15:25:10 -0400

    betterscript

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,37 @@
-var foodType = 'chicken'
+//const food1 = "chickn"
 
-fetch(
-    'https://api.edamam.com/api/nutrition-data?app_id=709d15ec&app_key=7d054cc543454bc65ec5a46c084b49fd&nutrition-type=logging&ingr='+foodType
-)
-.then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
+function getCals() {
+    let food1 = document.getElementById('food1').value
+    let food2 = document.getElementById('food2').value    
+    let food3 = document.getElementById('food3').value
+    let food4 = document.getElementById('food4').value
+ 
+    for (i=1;i<5;i++) {
 
-      console.log(data);
-    console.log(data.calories);
-    var kCal = data.calories;
-    console.log(kCal)
-  });
 
+    //let food1 = "chicken"
+ console.log(food1)
+
+
+ var foodQuery1 = 'https://api.edamam.com/api/nutrition-data?app_id=709d15ec&app_key=7d054cc543454bc65ec5a46c084b49fd&nutrition-type=logging&ingr='+food1
+
+
+ 
+ fetch(
+     foodQuery1
+ )
+ .then(function(response) {
+     return response.json();
+   })
+   .then(function(data) {
+ 
+       console.log(data);
+     console.log(data.calories);
+     var kCal = data.calories;
+     console.log(kCal)
+   });
+ }
+ }
+
+ 
 
