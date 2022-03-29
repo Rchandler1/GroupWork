5425835a24d7e0f5eee645fc5c2e375016bef540
Rchandler1 <98798057+Rchandler1@users.noreply.github.com>
2022-03-28 21:09:10 -0400

    testing

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,24 +1,23 @@
 //const food1 = "chickn"
 
 function getCals() {
+   
+    let food0 = "chicken"
     let food1 = document.getElementById('food1').value
     let food2 = document.getElementById('food2').value    
     let food3 = document.getElementById('food3').value
     let food4 = document.getElementById('food4').value
- 
+    let foods = [food0,food1,food2,food3,food4];
+    let foodCals = []
+
     for (i=1;i<5;i++) {
 
 
-    //let food1 = "chicken"
- console.log(food1)
-
-
- var foodQuery1 = 'https://api.edamam.com/api/nutrition-data?app_id=709d15ec&app_key=7d054cc543454bc65ec5a46c084b49fd&nutrition-type=logging&ingr='+food1
-
+ var foodQuery = 'https://api.edamam.com/api/nutrition-data?app_id=709d15ec&app_key=7d054cc543454bc65ec5a46c084b49fd&nutrition-type=logging&ingr='+foods[i]
 
  
  fetch(
-     foodQuery1
+     foodQuery 
  )
  .then(function(response) {
      return response.json();
@@ -26,12 +25,15 @@ function getCals() {
    .then(function(data) {
  
        console.log(data);
-     console.log(data.calories);
+
      var kCal = data.calories;
+     foodCals.push(kCal)
      console.log(kCal)
    });
  }
  }
+
+ 
 
  
 
